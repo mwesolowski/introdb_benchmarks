3849b977d61aa70ee6b4b5de7d642c953b009f27
Michal Wesolowski <wesolowski@gmail.com>
2018-12-19 00:23:45 +0100

    Bar charts generation

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -19,6 +19,7 @@ git fetch --all >> $detailed_log_file 2>&1
 echo "----- numberOfThreads: $numberOfThreads, poolSize: $poolSize -----"
 
 sorted_logins=$(echo -e "$logins" | sort  --ignore-case)
+> ../plot_data.txt
 for login in $sorted_logins; do
   git checkout ${login}/master >> $detailed_log_file 2>&1
   commit_on_master_at_specified_time=$(git rev-list -n 1 --before="$meeting_timestamp" ${login}/master)
@@ -33,10 +34,23 @@ for login in $sorted_logins; do
       : # echo "$login - build fails, probably tests do not pass"
     else
       echo $login
-      java -jar perf/target/benchmarks.jar -f 1 -t $numberOfThreads -p poolSize=$poolSize introdb.heap.pool.ObjectPoolBenchmark.testPool 2>/dev/null | tail -2
+      result="$(java -jar perf/target/benchmarks.jar -f 1 -t $numberOfThreads -p poolSize=$poolSize introdb.heap.pool.ObjectPoolBenchmark.testPool 2>/dev/null | tail -2)"
+      echo "$result"
+      last_line=$(echo -e "$result" | tail -n1)
+      operations_per_second=$(echo "$last_line" | tr -s ' ' | cut -d' ' -f5)
+      echo "$login $operations_per_second" >> ../plot_data.txt
     fi
     git checkout -- .
   fi 
 done
 
-cd --
+cd ..
+
+gnuplot -e "
+  set boxwidth 0.5;
+  set style fill solid;
+  set terminal jpeg;
+  set yrange [ 0 : * ];
+  set output 'benchmark_threads_${numberOfThreads}_pool_${poolSize}.jpeg';
+  plot 'plot_data.txt' using 2:xtic(1) with boxes title 'threads: ${numberOfThreads}, poolSize: ${poolSize}'
+"
